7ed9f5dd38e54bf1d56e6004fcdb7b058dfcc182
Josue Cruz Barragan <ing.cruzbarragan@hotmail.com>
2024-09-28 19:24:17 -0600

    menu de navegacion

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,32 @@
 import streamlit as st
 
-st.title("Dinamica Estructural de Edificios Simples")
-
-# Numero de pisos del edificio
-n_pisos =st.number_input("Número de pisos", min_value=1, step=1)
-
-modulo_elasticidad_all = st.radio("Modulo de Elasticidad de las columnas", ["Constante en todos los pisos", "Variable en todos los pisos"])
-if modulo_elasticidad_all == "Constante en todos los pisos":
-    modulo_elasticidad =st.number_input("Modulo de Elasticidad de las columnas:", min_value=1, step=1, key="modulo_elasticidad")
+#? --- PAGE SETUP ---#
+st.set_page_config(layout="wide")
 
 
-for piso in range(1, n_pisos + 1):
-    st.header(f"Piso {piso}")
-    col1, col2, col3 = st.columns([1,1,1])
-    with col1:
-        masa =st.number_input("Masa del piso por metro lineal", min_value=1, step=1, key=f"masa_{piso}")
-    with col2:
-        longitud =st.number_input("Longitud del piso:", min_value=0.0, step=0.1, key=f"longitud_{piso}")
-    with col3:
-        altura =st.number_input("Altura del piso:", min_value=0.0, step=0.1, key=f"altura_{piso}")
-    col4, col5, col6 = st.columns([1,1,1])
-    with col4:
-        momento_inercia =st.number_input("Momento de inercia de las columnas:", min_value=0.0, step=0.1, key=f"momento_inercia_{piso}")
-    with col5:
-        if modulo_elasticidad_all == "Variable en todos los pisos":
-            modulo_elasticidad = st.number_input("Mod. de Elast. de las columnas:", min_value=0.0, step=0.1, key=f"modulo_elasticidad_{piso}")
-    with col6:
-        tipo_columna = st.selectbox("Tipo de columna:", ["Empotrada", "Articulada"], key=f"tipo_columna_{piso}")
+data_page = st.Page(
+    page="views/crear_modelo.py",
+    title="Crear modelo",
+    icon="💾",
+    default=True
+)
+
+otro_page = st.Page(
+    page="views/otro.py",
+    title="Otro",
+    icon="💾"
+)
+
+#? --- NAVEGATION SETUP [WITH SECTIONS] ---#
+pg = st.navigation(
+    {
+        "Ingreso de datos": [data_page],
+    }
+)
+
+#? --- SHARE ON ALL PAGES ---#
+#st.logo("assets/code_ingeneiria.png")
+st.sidebar.text("Made by Code Ingeniería 🧑‍💻")
+
+#? --- RUN NAVEGATION ---#
+pg.run()
